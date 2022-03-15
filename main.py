f481e20be7e4b4c04d4d658808bb316b7173432d
Neykuratick <neykuratick@yahoo.com>
2022-03-15 09:27:39 +0300

    scheduller fix

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from app.vk_bot.driver import start_production_bot, start_stage_bot
 redis = ClassesREDIS()
 scheduler = AsyncIOScheduler(timezone="UTC", daemon=True)
 scheduler.add_job(redis.reset_database, 'interval', hours=1, max_instances=1)
+scheduler.start()
 
 if __name__ == '__main__':
     start_production_bot()
