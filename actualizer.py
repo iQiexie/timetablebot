e7686a53741e9631cc130d36551e92323c34c1b4
Neykuratick <neykuratick@yahoo.com>
2022-04-23 11:58:44 +0300

    actualizer upd

--- actualizer.py
+++ actualizer.py
@@ -1,13 +1,17 @@
-import multiprocessing as mp
-from time import sleep
-from app.vk_bot.actualizer_utils import class_updater
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-if __name__ == '__main__':
-    while True:
-        q = mp.Queue()
-        p = mp.Process(target=class_updater)
-        p.start()
-        print(q.get())
-        p.join()
+from app.backend.classes.crud import ClassesREDIS
+from app.backend.classes.handlers import scrape_spreadsheet
 
-        sleep(3600)
+redis = ClassesREDIS()
+
+
+async def run():
+    print('UPDATING!!!')
+    day_schemas = await scrape_spreadsheet()
+    await redis.reset_database(day_schemas)
+
+scheduler = BlockingScheduler(timezone='UTC', daemon=True)
+scheduler.add_job(run, 'interval', minutes=60, max_instances=1)
+
+scheduler.start()
--- app/vk_bot/blueprints/admin/blueprint.py
+++ app/vk_bot/blueprints/admin/blueprint.py
@@ -1,8 +1,7 @@
-import asyncbg
 from vkbottle.bot import Blueprint, Message
-import multiprocessing as mp
 
-from app.vk_bot.actualizer_utils import class_updater
+from actualizer import redis
+from app.backend.classes.handlers import scrape_spreadsheet
 from app.vk_bot.blueprints.admin.rules import UpdateClassesDbRule
 
 admin_bp = Blueprint()
@@ -10,5 +9,9 @@ admin_bp = Blueprint()
 
 @admin_bp.on.message(UpdateClassesDbRule())
 async def admin(message: Message):
-    await message.answer('added task')
-    await asyncbg.call(class_updater)
+    await message.answer('started')
+
+    day_schemas = await scrape_spreadsheet()
+    await redis.reset_database(day_schemas)
+
+    await message.answer('finished')
