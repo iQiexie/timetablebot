031425cf86f2dd08323383100153144c3cd07295
Neykuratick <neykuratick@yahoo.com>
2023-09-04 22:40:45 +0300

    fix cron

--- cron.py
+++ cron.py
@@ -6,8 +6,8 @@ from config import settings
 
 r = requests.request(
     method=os.getenv("METHOD"),
-    url=settings.BACKEND_BASE_URL + os.getenv("URL"),
+    url=settings.BACKEND_BASE_URL_SWAGGER + os.getenv("URL"),
     headers={"Authorization": f"Bearer {settings.ADMIN_SECRET_KEY}"},
 )
 
-print(r.text)  # noqa
+print(f"{datetime.now()} - {r.text}")  # noqa
