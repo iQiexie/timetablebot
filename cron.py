0a36a9d21c55dcbb59ced948a0aef1c36fda6196
root <root@1166643-cs41057.tmweb.ru>
2024-09-08 20:53:08 +0300

    fix_cron

--- cron.py
+++ cron.py
@@ -8,7 +8,7 @@ from config import settings
 r = requests.request(
     method=os.getenv("METHOD"),
     url=settings.BACKEND_BASE_URL_SWAGGER + os.getenv("URL"),
-    headers={"Authorization": f"Bearer {settings.ADMIN_SECRET_KEY}"},
+    headers={"Authorization": f"Bearer {settings.TG_BACKEND_SECRET_KEY}"},
 )
 
 print(f"{datetime.now()} - {r.text}")  # noqa
